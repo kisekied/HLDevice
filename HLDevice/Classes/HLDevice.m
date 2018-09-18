7070b8c085353dd5e0bc46f19741ae41b8819ff5
kisekied <kisekied@gmail.com>
2018-09-18 14:39:41 +0800

    Change name

--- HLDevice/Classes/HLDevice.m
+++ HLDevice/Classes/HLDevice.m
@@ -21,6 +21,7 @@
 
 - (instancetype)init {
     if (self = [super init]) {
+
         [self mapToDeviceModel];
     }
     return self;
@@ -39,18 +40,18 @@
 }
 
 - (BOOL)isPhone {
-    return (self.deviceModel >= HLDeviceModel_iPhoneMinValue &&
-            self.deviceModel <= HLDeviceModel_iPhoneMaxValue);
+    return (self.deviceModel >= HLDeviceModeliPhoneMinValue &&
+            self.deviceModel <= HLDeviceModeliPhoneMaxValue);
 }
 
 - (BOOL)isPad {
-    return (self.deviceModel >= HLDeviceModel_iPadMinValue &&
-            self.deviceModel <= HLDeviceModel_iPadMaxValue);
+    return (self.deviceModel >= HLDeviceModeliPadMinValue &&
+            self.deviceModel <= HLDeviceModeliPadMaxValue);
 }
 
 - (BOOL)isPod {
-    return (self.deviceModel >= HLDeviceModel_iPodMinValue &&
-            self.deviceModel <= HLDeviceModel_iPodMaxValue);
+    return (self.deviceModel >= HLDeviceModeliPodMinValue &&
+            self.deviceModel <= HLDeviceModeliPodMaxValue);
 }
 
 
@@ -60,7 +61,7 @@
     if ([self.identifier isEqualToString:@"i386"] || [self.identifier isEqualToString:@"x86_64"]) {
         _deviceType     = HLDeviceTypeSimulator;
         self.identifier =
-        [NSProcessInfo processInfo].environment[@"SIMULATOR_MODEL_IDENTIFIER"] ?: @"iOS";
+        [NSProcessInfo processInfo].environment[@"SIMULATOR_ModelIDENTIFIER"] ?: @"iOS";
     } else {
         _deviceType     = HLDeviceTypeRealMachine;
     }
@@ -76,7 +77,7 @@
         }
     }
 
-    _deviceModel = HLDeviceModel_Unknown;
+    _deviceModel = HLDeviceModelUnknown;
     _deviceType  = HLDeviceTypeUnkown;
 }
 
@@ -99,79 +100,79 @@
     dispatch_once(&onceToken, ^{
         dict = @{
                  // iPhone
-                 @"iPhone1,1": @(HLDeviceModel_iPhone1G),
-                 @"iPhone1,2": @(HLDeviceModel_iPhone3G),
-                 @"iPhone2,1": @(HLDeviceModel_iPhone3GS),
-                 @"iPhone3,1": @(HLDeviceModel_iPhone4),
-                 @"iPhone3,2": @(HLDeviceModel_iPhone4),
-                 @"iPhone3,3": @(HLDeviceModel_iPhone4),
-                 @"iPhone4,1": @(HLDeviceModel_iPhone4s),
-                 @"iPhone5,1": @(HLDeviceModel_iPhone5),
-                 @"iPhone5,2": @(HLDeviceModel_iPhone5),
-                 @"iPhone5,3": @(HLDeviceModel_iPhone5c),
-                 @"iPhone5,4": @(HLDeviceModel_iPhone5c),
-                 @"iPhone6,1": @(HLDeviceModel_iPhone5s),
-                 @"iPhone6,2": @(HLDeviceModel_iPhone5s),
-                 @"iPhone7,2": @(HLDeviceModel_iPhone6),
-                 @"iPhone7,1": @(HLDeviceModel_iPhone6Plus),
-                 @"iPhone8,1": @(HLDeviceModel_iPhone6s),
-                 @"iPhone8,2": @(HLDeviceModel_iPhone6sPlus),
-                 @"iPhone9,1": @(HLDeviceModel_iPhone7),
-                 @"iPhone9,3": @(HLDeviceModel_iPhone7),
-                 @"iPhone9,2": @(HLDeviceModel_iPhone7Plus),
-                 @"iPhone9,4": @(HLDeviceModel_iPhone7Plus),
-                 @"iPhone8,4": @(HLDeviceModel_iPhoneSE),
-                 @"iPhone10,1": @(HLDeviceModel_iPhone8),
-                 @"iPhone10,4": @(HLDeviceModel_iPhone8),
-                 @"iPhone10,2": @(HLDeviceModel_iPhone8Plus),
-                 @"iPhone10,5": @(HLDeviceModel_iPhone8Plus),
-                 @"iPhone10,3": @(HLDeviceModel_iPhoneX),
-                 @"iPhone10,6": @(HLDeviceModel_iPhoneX),
+                 @"iPhone1,1": @(HLDeviceModeliPhone1G),
+                 @"iPhone1,2": @(HLDeviceModeliPhone3G),
+                 @"iPhone2,1": @(HLDeviceModeliPhone3GS),
+                 @"iPhone3,1": @(HLDeviceModeliPhone4),
+                 @"iPhone3,2": @(HLDeviceModeliPhone4),
+                 @"iPhone3,3": @(HLDeviceModeliPhone4),
+                 @"iPhone4,1": @(HLDeviceModeliPhone4s),
+                 @"iPhone5,1": @(HLDeviceModeliPhone5),
+                 @"iPhone5,2": @(HLDeviceModeliPhone5),
+                 @"iPhone5,3": @(HLDeviceModeliPhone5c),
+                 @"iPhone5,4": @(HLDeviceModeliPhone5c),
+                 @"iPhone6,1": @(HLDeviceModeliPhone5s),
+                 @"iPhone6,2": @(HLDeviceModeliPhone5s),
+                 @"iPhone7,2": @(HLDeviceModeliPhone6),
+                 @"iPhone7,1": @(HLDeviceModeliPhone6Plus),
+                 @"iPhone8,1": @(HLDeviceModeliPhone6s),
+                 @"iPhone8,2": @(HLDeviceModeliPhone6sPlus),
+                 @"iPhone9,1": @(HLDeviceModeliPhone7),
+                 @"iPhone9,3": @(HLDeviceModeliPhone7),
+                 @"iPhone9,2": @(HLDeviceModeliPhone7Plus),
+                 @"iPhone9,4": @(HLDeviceModeliPhone7Plus),
+                 @"iPhone8,4": @(HLDeviceModeliPhoneSE),
+                 @"iPhone10,1": @(HLDeviceModeliPhone8),
+                 @"iPhone10,4": @(HLDeviceModeliPhone8),
+                 @"iPhone10,2": @(HLDeviceModeliPhone8Plus),
+                 @"iPhone10,5": @(HLDeviceModeliPhone8Plus),
+                 @"iPhone10,3": @(HLDeviceModeliPhoneX),
+                 @"iPhone10,6": @(HLDeviceModeliPhoneX),
                  // iPad
-                 @"iPad1,1": @(HLDeviceModel_iPad1),
-                 @"iPad2,1": @(HLDeviceModel_iPad2),
-                 @"iPad2,2": @(HLDeviceModel_iPad2),
-                 @"iPad2,3": @(HLDeviceModel_iPad2),
-                 @"iPad2,4": @(HLDeviceModel_iPad2),
-                 @"iPad3,1": @(HLDeviceModel_iPad3),
-                 @"iPad3,2": @(HLDeviceModel_iPad3),
-                 @"iPad3,3": @(HLDeviceModel_iPad3),
-                 @"iPad3,4": @(HLDeviceModel_iPad4),
-                 @"iPad3,5": @(HLDeviceModel_iPad4),
-                 @"iPad3,6": @(HLDeviceModel_iPad4),
-                 @"iPad4,1": @(HLDeviceModel_iPadAir),
-                 @"iPad4,2": @(HLDeviceModel_iPadAir),
-                 @"iPad4,3": @(HLDeviceModel_iPadAir),
-                 @"iPad5,3": @(HLDeviceModel_iPadAir2),
-                 @"iPad5,4": @(HLDeviceModel_iPadAir2),
-                 @"iPad6,11": @(HLDeviceModel_iPad5),
-                 @"iPad6,12": @(HLDeviceModel_iPad5),
-                 @"iPad2,5": @(HLDeviceModel_iPadMini),
-                 @"iPad2,6": @(HLDeviceModel_iPadMini),
-                 @"iPad2,7": @(HLDeviceModel_iPadMini),
-                 @"iPad4,4": @(HLDeviceModel_iPadMini2),
-                 @"iPad4,5": @(HLDeviceModel_iPadMini2),
-                 @"iPad4,6": @(HLDeviceModel_iPadMini2),
-                 @"iPad4,7": @(HLDeviceModel_iPadMini3),
-                 @"iPad4,8": @(HLDeviceModel_iPadMini3),
-                 @"iPad4,9": @(HLDeviceModel_iPadMini3),
-                 @"iPad5,1": @(HLDeviceModel_iPadMini4),
-                 @"iPad5,2": @(HLDeviceModel_iPadMini4),
-                 @"iPad6,3": @(HLDeviceModel_iPadPro9Inch),
-                 @"iPad6,4": @(HLDeviceModel_iPadPro9Inch),
-                 @"iPad6,7": @(HLDeviceModel_iPadPro12Inch),
-                 @"iPad6,8": @(HLDeviceModel_iPadPro12Inch),
-                 @"iPad7,1": @(HLDeviceModel_iPadPro12Inch2),
-                 @"iPad7,2": @(HLDeviceModel_iPadPro12Inch2),
-                 @"iPad7,3": @(HLDeviceModel_iPadPro10Inch),
-                 @"iPad7,4": @(HLDeviceModel_iPadPro10Inch),
+                 @"iPad1,1": @(HLDeviceModeliPad1),
+                 @"iPad2,1": @(HLDeviceModeliPad2),
+                 @"iPad2,2": @(HLDeviceModeliPad2),
+                 @"iPad2,3": @(HLDeviceModeliPad2),
+                 @"iPad2,4": @(HLDeviceModeliPad2),
+                 @"iPad3,1": @(HLDeviceModeliPad3),
+                 @"iPad3,2": @(HLDeviceModeliPad3),
+                 @"iPad3,3": @(HLDeviceModeliPad3),
+                 @"iPad3,4": @(HLDeviceModeliPad4),
+                 @"iPad3,5": @(HLDeviceModeliPad4),
+                 @"iPad3,6": @(HLDeviceModeliPad4),
+                 @"iPad4,1": @(HLDeviceModeliPadAir),
+                 @"iPad4,2": @(HLDeviceModeliPadAir),
+                 @"iPad4,3": @(HLDeviceModeliPadAir),
+                 @"iPad5,3": @(HLDeviceModeliPadAir2),
+                 @"iPad5,4": @(HLDeviceModeliPadAir2),
+                 @"iPad6,11": @(HLDeviceModeliPad5),
+                 @"iPad6,12": @(HLDeviceModeliPad5),
+                 @"iPad2,5": @(HLDeviceModeliPadMini),
+                 @"iPad2,6": @(HLDeviceModeliPadMini),
+                 @"iPad2,7": @(HLDeviceModeliPadMini),
+                 @"iPad4,4": @(HLDeviceModeliPadMini2),
+                 @"iPad4,5": @(HLDeviceModeliPadMini2),
+                 @"iPad4,6": @(HLDeviceModeliPadMini2),
+                 @"iPad4,7": @(HLDeviceModeliPadMini3),
+                 @"iPad4,8": @(HLDeviceModeliPadMini3),
+                 @"iPad4,9": @(HLDeviceModeliPadMini3),
+                 @"iPad5,1": @(HLDeviceModeliPadMini4),
+                 @"iPad5,2": @(HLDeviceModeliPadMini4),
+                 @"iPad6,3": @(HLDeviceModeliPadPro9Inch),
+                 @"iPad6,4": @(HLDeviceModeliPadPro9Inch),
+                 @"iPad6,7": @(HLDeviceModeliPadPro12Inch),
+                 @"iPad6,8": @(HLDeviceModeliPadPro12Inch),
+                 @"iPad7,1": @(HLDeviceModeliPadPro12Inch2),
+                 @"iPad7,2": @(HLDeviceModeliPadPro12Inch2),
+                 @"iPad7,3": @(HLDeviceModeliPadPro10Inch),
+                 @"iPad7,4": @(HLDeviceModeliPadPro10Inch),
                  // iPod
-                 @"iPod1,1": @(HLDeviceModel_iPodTouch1),
-                 @"iPod2,1": @(HLDeviceModel_iPodTouch2),
-                 @"iPod3,1": @(HLDeviceModel_iPodTouch3),
-                 @"iPod4,1": @(HLDeviceModel_iPodTouch4),
-                 @"iPod5,1": @(HLDeviceModel_iPodTouch5),
-                 @"iPod7,1": @(HLDeviceModel_iPodTouch6),
+                 @"iPod1,1": @(HLDeviceModeliPodTouch1),
+                 @"iPod2,1": @(HLDeviceModeliPodTouch2),
+                 @"iPod3,1": @(HLDeviceModeliPodTouch3),
+                 @"iPod4,1": @(HLDeviceModeliPodTouch4),
+                 @"iPod5,1": @(HLDeviceModeliPodTouch5),
+                 @"iPod7,1": @(HLDeviceModeliPodTouch6),
                  };
     });
     return dict;
